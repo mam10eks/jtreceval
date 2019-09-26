9ee097c7e7290cbdce2b1b7fbd7aebc32f0d5d05
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2019-09-26 12:50:02 +0200

    Add basic test for gdeval and ndeval

--- src/test/java/uk/ac/gla/terrier/jtreceval/TestItRuns.java
+++ src/test/java/uk/ac/gla/terrier/jtreceval/TestItRuns.java
@@ -6,8 +6,28 @@ import org.junit.Test;
 
 public class TestItRuns 
 {
-	@Test public void testBasic()
+	@Test public void testBasicTrecEval()
 	{
 		assertEquals(0, new trec_eval().run(new String[]{"-h"}));
+	}
+	
+	@Test public void testBasicGdeval()
+	{
+		new gdeval().runAndGetOutput(new String[] {"-help"});
+	}
+	
+	@Test(expected=RuntimeException.class) public void testBasicGdevalFails()
+	{
+		new gdeval().runAndGetOutput(new String[] {"NOT_AN_ARGUMENT"});
+	}
+	
+	@Test public void testBasicNdeval()
+	{
+		new ndeval().runAndGetOutput(new String[] {"-help"});
+	}
+	
+	@Test(expected=RuntimeException.class) public void testBasicNdevalFails()
+	{
+		new ndeval().runAndGetOutput(new String[] {"NOT_AN_ARGUMENT"});
 	}
 }
